2fd48d915a4bd1301ef6c83e42d88c0408813f69
fedor572 <fedorsakhnin@gmail.com>
2024-10-06 13:06:06 +0300

    Add files via upload

---
+++ create7.py
@@ -0,0 +1,23 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+
+values_list = (2, False, 'привет')
+values_dict = {'a': 3, 'b': True, 'c': 'пока'}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+
+def append_to_list(item, values_list=None):
+    if values_list is None:
+        values_list = []
+        values_list.append(item)
+
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
